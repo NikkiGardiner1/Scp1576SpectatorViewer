7eda0bad678e5a3ebf929cfe4b35ab20c7ba01c7
Nikki Gardiner <mill385@yahoo.com>
2023-09-04 14:16:01 -0400

    Updated the required Exiled Version to 8.1.0

--- Scp1576SpectatorViewer/MainClass.cs
+++ Scp1576SpectatorViewer/MainClass.cs
@@ -6,11 +6,11 @@ namespace Scp1576SpectatorViewer;
 
 public class MainClass : Plugin<Config>
 {
-    public override string Author { get; } = "xNexusACS";
+    public override string Author { get; } = "Vicious Vikki";
     public override string Name { get; } = "Scp1576SpectatorViewer";
     public override string Prefix { get; } = "Scp1576SpectatorViewer";
     public override Version Version { get; } = new(1, 0, 1);
-    public override Version RequiredExiledVersion { get; } = new(7, 0, 0);
+    public override Version RequiredExiledVersion { get; } = new(8, 1, 0);
     
     public Harmony Harmony { get; private set; }
     
